612ce0f67fac432c8c77c01504ded33d45605966
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-02-07 14:04:31 +0530

    ...

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,3 +1,24 @@
+
+import java.util.Arrays;
+
+
 public class ReverseArray {
-  
+
+    public static void main(String[] args) {
+        int arr[] = ArrayUtility.arrInp();
+
+        reverse(arr);
+
+    }
+
+    public static void reverse(int[] arr) {
+        int i = 0;
+        while (i < arr.length / 2) {
+            int swap = arr[i];
+            arr[i] = arr[(arr.length - 1) - i];
+            arr[(arr.length - 1) - i] = swap;
+            i++;
+        }
+        System.out.println("Array is " + Arrays.toString(arr));
+    }
 }
