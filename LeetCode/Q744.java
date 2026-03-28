086322ff06cdd3f0f25302a88d5c630c4452af75
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-03-28 20:24:34 +0530

    feat: Update `Q744`

--- LeetCode/Q744.java
+++ LeetCode/Q744.java
@@ -1,26 +1,27 @@
 public class Q744 {
   public static void main(String[] args) {
     char[] letters = { 'c', 'f', 'j' };
-    char target = 'a';
+    char target = 'f';
 
     char result = nextGreatestLetter(letters, target);
     System.out.println(result);
   }
 
   public static char nextGreatestLetter(char[] letters, char target) {
-    int start = 0, end = letters.length - 1;
 
-    while (start <= end) {
+    int start = 0;
+    int end = letters.length - 1;
+
+    while(start <= end){
       int mid = start + (end - start) / 2;
 
-      if (letters[mid] <= target) {
-        start = mid + 1;
-      } else {
+      if(target < letters[mid]){
         end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
-
-    // wrap-around case
     return letters[start % letters.length];
   }
+  
 }
